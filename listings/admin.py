a708b8eed54fb5d73b40d7b21e05745aa8d40f77
rjptechsolutions <rohaanjosh@gmail.com>
2018-11-05 12:38:54 +0530

    final done project

---
+++ listings/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','city','state','is_published','price','list_date','realtor')
+    list_display_links = ('id','title','price')
+    list_filter =('realtor','state',)
+    list_editable =('is_published',)
+    search_fields =('title','description','address','city','state','zipcode','price')
+    list_per_page =25
+admin.site.register(Listing, ListingAdmin)
